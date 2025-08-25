94df3149fcf7783487ffd2b61e3ce0dea09bb703
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2025-08-25 10:07:05 -0500

    router: redirect unknown paths to /home

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { Navigate } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   component: () => <Outlet />,
+  notFoundComponent: () => <Navigate to="/home" replace />,
 }); 
