8bad30a4c2006974474581a9732ec96194cfc74d
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2025-08-25 20:47:43 -0500

    Removed the unnecessary wrapper function in the route

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,15 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { HomePage } from '../features/home/home.page';
-import { MainLayout } from '../components/layout/MainLayout';
 
 export const Route = createFileRoute('/home')({
-  component: Home,
-});
-
-function Home() {
-  return (
-    <MainLayout>
-      <HomePage />
-    </MainLayout>
-  );
-} 
+  component: HomePage,
+}); 
