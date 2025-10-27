a36c763c39d691f70f6dc53606bcce1163ca8529
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2025-10-27 14:27:36 -0500

    Refactor useGetAllFolders hook to remove unused useEffect import

--- src/features/folder/hooks/useGetAllFolders.ts
+++ src/features/folder/hooks/useGetAllFolders.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { API_URL } from '../../../utils/constants';
 import { getAuthToken, buildAuthHeaders } from '../../../utils/auth-headers';
 import { useAuthContext } from '../../../utils/auth/use-auth-context';
